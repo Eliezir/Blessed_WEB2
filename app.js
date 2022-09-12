3f4a9b77aedd197def9e9b098c82e3a1829983db
ELIEZIR MOREIRA PEIXOTO NETO <2019311359@IFAL.LOCAL>
2022-09-12 11:41:53 -0300

    express js

---
+++ app.js
@@ -0,0 +1,34 @@
+
+var express = require('express');
+var load = require("express-load")
+var path = require('path');
+
+
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+load('models')
+.then('controllers')
+.then('routes')
+.into(app);
+
+app.listen(3000,function(){
+  console.log("tudo certo, OK!")
+})
+
+
+
+module.exports = app;
