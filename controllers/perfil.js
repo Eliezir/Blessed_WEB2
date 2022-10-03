c907a4ca162cbd75720528c05c91241515da71e6
Eliezir Moreira <eliezir2004@gmail.com>
2022-10-02 23:05:28 -0300

    novas ações para as rotas

--- controllers/perfil.js
+++ controllers/perfil.js
@@ -5,6 +5,37 @@ module.exports = function(app){
             let params = {usuario: usuario}
             res.render("perfil/index", params)
         },
+        create: function(req, res){
+            var comentario = req.body.comentario,
+            usuario = req.session.ususario;
+            usuario.comentarios.push(comentario);
+            res.redirect("/perfil")
+        },
+        comentarios: function(req, res) {
+            var id = req.params.id
+            comentario = req.session.usuario.comentario[id],
+            parametro = {contato: contato, id:id};
+            res.render("comentarios/show", params);
+        },
+        edit: function (req, res) {
+            var id = req.params.id,
+            usuario = req.session.usuario,
+            comentario = usuario.comentario[id],
+            params = { usuario: usuario, comentario: comentario, id: id };
+            res.render("comentario/edit", params);
+            },
+            update: function (req, res) {
+                var comentario = req.body.comentario,
+                usuario = req.session.usuario;
+                usuario.comentarios[req.params.id] = comentario;
+                res.redirect("/comentarios");
+                },
+            destroy: function (req, res) {
+                var usuario = req.session.usuario,
+                id = req.params.id;
+                usuario.comentarios.splice(id, 1);
+                res.redirect("/comentarios");
+                },
     }
     return PerfilController;
 }
--- routes/perfil.js
+++ routes/perfil.js
@@ -1,4 +1,7 @@
 module.exports = function(app){
     let perfil = app.controllers.perfil;
     app.get('/perfil', perfil.index )
+    app.post("/perfil/:id/comentario", perfil.comentarios)
+    app.get("/perfil/:id/editar", perfil.edit)
+    app.delete("/perfil/:id/comentario", perfil.destroy);
 };
