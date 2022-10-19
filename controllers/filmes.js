17b6bd5215869ec323d47956d6d6d4ddb6f06cf6
ELIEZIR MOREIRA PEIXOTO NETO <2019311359@IFAL.LOCAL>
2022-10-19 08:28:09 -0300

    edit navbar

--- controllers/filmes.js
+++ controllers/filmes.js
@@ -2,19 +2,23 @@ module.exports = function(app){
     var filmeController = {
         adam: function(req,res){
             //acesso a pagina adam sandler
-            res.render("filmes/adam")
+            usuario = req.session.usuario;
+            res.render("filmes/adam",usuario)
         },
         lancamento: function(req,res){
-            //acesso a pagina adam sandler
-            res.render("filmes/lancamento")
+            //acesso a pagina lançamentos
+            usuario = req.session.usuario;
+            res.render("filmes/lancamento",usuario)
         },
         cliches: function(req,res){
-            //acesso a pagina adam sandler
-            res.render("filmes/cliches")
+            //acesso a pagina filmes clichês
+            usuario = req.session.usuario;
+            res.render("filmes/cliches",usuario)
         },
         nacional: function(req,res){
-            //acesso a pagina adam sandler
-            res.render("filmes/nacional")
+            //acesso a pagina filmes nacionais
+            usuario = req.session.usuario;
+            res.render("filmes/nacional",usuario)
         }, 
         
     };
